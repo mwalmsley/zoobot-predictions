d2fc34f358ee2063fcc468b4c5dd88b4f9981271
Mike Walmsley <walmsleymk1@gmail.com>
2024-03-31 15:15:03 -0400

    wrong python

--- make_predictions/make_representations/to_friendly_table.sh
+++ make_predictions/make_representations/to_friendly_table.sh
@@ -7,7 +7,8 @@
 
 pwd; hostname; date
 
-PYTHON=/share/nas2/walml/miniconda3/envs/zoobot38_torch/bin/python
+# PYTHON=/share/nas2/walml/miniconda3/envs/zoobot38_torch/bin/python
+PYTHON=/home/walml/envs/zoobot39_dev/bin/python
 
 # CHECKPOINT_NAME=evo_py_gr_eff_224_814
 # CHECKPOINT_NAME=evo_py_gr_vittiny_224_19505
