272cabba41f6e2b82c8d72d95dcd083d7a4ffd52
Mike Walmsley <walmsleymk1@gmail.com>
2024-03-31 15:23:45 -0400

    path tinkering

--- make_predictions/make_representations/pca_table.sh
+++ make_predictions/make_representations/pca_table.sh
@@ -17,13 +17,13 @@ COMPONENTS=30
 # CHECKPOINT_NAME=evo_py_co_vittiny_224_7325
 CHECKPOINT_NAME=convnext_nano_evo
 
-# PREDICTIONS_DIR=/share/nas2/walml/galaxy_zoo/decals/dr8/representations
-PREDICTIONS_DIR=/project/def-bovy/walml/repos/zoobot-predictions/data/desi_wds/representations
-
-PARQUET_LOC=${PREDICTIONS_DIR}/${CHECKPOINT_NAME}_representations.parquet
-SAVE_LOC=${PREDICTIONS_DIR}/${CHECKPOINT_NAME}_representations_pca_${COMPONENTS}.parquet
-
 REPO_DIR=/project/def-bovy/walml/repos/zoobot-predictions
+
+PREDICTIONS_DIR=$REPO_DIR/data/desi_wds/representations
+
+PARQUET_LOC=${PREDICTIONS_DIR}/${CHECKPOINT_NAME}/representations.parquet
+SAVE_LOC=${PREDICTIONS_DIR}/${CHECKPOINT_NAME}/representations_pca_${COMPONENTS}.parquet
+
 
 $PYTHON $REPO_DIR/make_predictions/make_representations/pca_table.py \
     --parquet-loc $PARQUET_LOC \
